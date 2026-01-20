35cb11708186b3d9f369c471d4fb948bc8884b06
Vitali3207 <makar3447@gmail.com>
2026-01-20 15:29:58 +0300

    исправления

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -4,11 +4,11 @@ const navToggle = document.querySelector('.main-nav__toggle');
 navMain.classList.remove('main-nav--nojs');
 
 navToggle.addEventListener('click', () => {
-  if (navMain.classList.contains('main-nav--closed')) {
-    navMain.classList.remove('main-nav--closed');
+  if (navMain.classList.contains('main-nav')) {
+    navMain.classList.remove('main-nav');
     navMain.classList.add('main-nav--opened');
   }else {
-    navMain.classList.add('main-nav--closed');
+    navMain.classList.add('main-nav');
     navMain.classList.remove('main-nav--opened');
   }
 });
